5aeb45068315c9a86b7dff2a01dc44b68ac535da
chikuwa111 <take0shun1111@gmail.com>
2021-11-03 17:45:43 +0900

    Add style and script

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,29 @@
+const update = (scrollY: number) => {
+  document.getElementById("hello").style.color = "blue";
+}
+
 document.addEventListener('DOMContentLoaded', () => {
-  document.getElementById('hello').style.color = 'blue'
+  update(window.scrollY);
+});
+
+/* 
+  Reference: 
+    https://developer.mozilla.org/ja/docs/Web/API/Document/scroll_event,
+    https://www.html5rocks.com/en/tutorials/speed/animations/
+*/
+
+let last_known_scroll_position = 0;
+let ticking = false;
+
+window.addEventListener("scroll", () => {
+  last_known_scroll_position = window.scrollY;
+
+  if (!ticking) {
+    window.requestAnimationFrame(() => {
+      update(last_known_scroll_position);
+      ticking = false;
+    });
+
+    ticking = true;
+  }
 });
